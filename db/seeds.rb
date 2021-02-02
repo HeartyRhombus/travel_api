0a7d67114248dbc112f07b8ca1172c8bab886513
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 09:33:37 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(city: "Paris", country: "France", visited: true)
+Place.create(city: "Sydney", country: "Australia", visited: false)
+Place.create(city: "Tokyo", country: "Japan", visited: false)
+
+Event.find_or_create_by(name: "Eiffel Tower").update_attributes(visited: true, place_id: 1 )
+Event.find_or_create_by(name: "Sydney Opera House").update_attributes(visited: false, place_id: 2)
+Event.find_or_create_by(name: "Cherry Blossom Festival").update_attributes(visited: false, place_id: 3)
